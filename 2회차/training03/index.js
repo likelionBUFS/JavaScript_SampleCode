42b335a84699002321bdf767fe3ab457497cbd28
SugarSyrup <tlfvm04@naver.com>
2023-05-11 11:49:54 +0900

    training03

---
+++ 2회차/training03/index.js
@@ -0,0 +1,10 @@
+const plusButton = document.querySelector('button.plus');
+const minusButton = document.querySelector('button.minus');
+
+plusButton.addEventListener('click', () => {
+    //plusButton 누르면?
+})
+
+minusButton.addEventListener('click', () => {
+    //minusButton 누르면?
+})
