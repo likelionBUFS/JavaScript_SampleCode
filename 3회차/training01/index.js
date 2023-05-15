aff6b2b24bd8f45d85a3b7daebcaaff98bf64574
여다희 <yeodahui.dev@gmail.com>
2023-05-15 17:10:44 +0900

    예제 코드 변경

--- 3회차/training01/index.js
+++ 3회차/training01/index.js
@@ -1,11 +1,12 @@
 /* DOM 요소 불러오기 */
-const form = document.querySelector(".form");
-const scoreInput = form.querySelector(".score");
-const submitBtn = form.querySelector(".submitBtn");
+/**@TODO : score 인풋과 제출 btn 불러오기 */
+const scoreInput = ;
+const submitBtn = ;
 
 submitBtn.addEventListener("click", (e) => {
   e.preventDefault(); // submit했을 때 새로고침되는 현상을 막습니다.
-  const score = scoreInput.innerText; // 입력한 점수를 불러옵니다.
+  
+  /** @TODO : scoreInput에 입력된 점수를 불러오기 */
 
   /** @TODO : 조건문 만들어 학점 출력하기 */
   /**
