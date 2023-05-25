5e69205cbcc8352763e7387cc0e4fb00987ebd10
여다희 <yeodahui.dev@gmail.com>
2023-05-25 15:54:49 +0900

    버튼 자바스크립트 예제

---
+++ 4회차/buttonJS/index2.js
@@ -0,0 +1,14 @@
+/* DOM */
+const checkBox = document.querySelector("#inputDisabled");
+const button = document.querySelector("#button");
+
+/* Event Listener */
+checkBox.addEventListener("change", (e) => {
+  // 체크박스 클릭했을 때 발생하는 "change" 이벤트에 따른 효과를 등록합니다.
+  console.log("changed");
+
+  // 버튼 클래스에 disalbed가 있는가?
+  // 있다면, "disabled" 클래스 제거
+  // 없다면, "disabled" 클래스 추가
+  button.classList.toggle("disabled");
+});
